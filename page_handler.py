b86e2f93e5eca6d67dd7cede79a62845874e2a17
jhamilt5 <jessehamilton4@gmail.com>
2015-10-01 11:02:46 -0400

    Added a dummy resource for testing JSON receiving

--- page_handler.py
+++ page_handler.py
@@ -44,5 +44,13 @@ class Page_Handler():
     def get_guest_request_phrase_html(self):
         return env.get_template('GuestRequestPhrase.html').render()
     
-    def get_guest_game_html(self, game_id):
-        return env.get_template('GuestGame.html').render(game_id=game_id)
+    def get_guest_game_html(self, gid):
+        return env.get_template('GuestGame.html').render(gid=gid)
+    
+    def get_dummy_game(self, gid):
+        result = {}
+        result["answer"] = "this is a test phrase"
+        result["incorrect_letters"] = ['u','b','c']
+        result["incorrect_words"] = ['wronge guess']
+        result["correct_letters"] = ['s','i','t','a']
+        return json.dumps(result)
