cc5266aceb5dfa4827ff6a843278c7b2a746ecbb
user <rboccabe@nd.edu>
2015-10-08 15:51:08 -0400

    gameplay.js

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,4 +1,43 @@
 $(function() {
+    $(document).ready({
+        $.ajax({
+            type : 'GET',
+            url : '/game/'+uid+'/'+gid,
+            contentType: 'application/json',
+        }).done(function(data){
+            var d = data;
+            var dict = json.parse(d);
+            var win_uid = dict['win']
+            var guess_uid = dict['guesser_uid']
+            var creator_uid = dict['creator_uid']
+            var message = ""
+            var display = true;
+            if(win_uid == uid){
+                if(uid == guess_uid){
+                    message = "You correctly guessed the phrase!";
+                }
+                else{
+                    message = "Your opponent failed to guess your word!"
+                }
+            }
+            else if(win_uid == guess_uid){
+                message = "Your opponent correctly guessed your phrase.";
+            }
+            else if(win_uid == creator_uid){
+                message = "You failed ot guess your opponent's phrase."
+            }
+            else{
+                display = false;
+            }
+            
+            if(display){
+                window.alert(message)
+                                
+				window.location.href = "/guestlobby/"+uid;
+            }
+        })
+    });
+
 	$('#guess-form').on('submit', function(event) {
 		
 		event.preventDefault();
