bcce3af1e5b5204210e55dbc7c1f76335d2efa5c
user <rboccabe@nd.edu>
2015-10-08 14:53:48 -0400

    wrong file last commit: game_handler.py: add error checking for gid and uid error checking for guess

--- game_handler.py
+++ game_handler.py
@@ -22,14 +22,22 @@ class Game_Handler():
         result["incorrect_letters"] = ['u','b','c']
         result["incorrect_words"] = ['wronge guess']
         result["correct_letters"] = ['s','i','t','a']
+        result["guesser_uid"] = 'u1'
+        result["creator_uid"] = 'u2'
+        result["errors"] = []
+        result["result"] = "Success"
         return json.dumps(result)
     
-    def post_guess(self, uid, gid, letterguess):
-        '''data_in = cherrypy.request.body.read()
-        data_json = json.loads(data_in)'''
+    def post_guess(self, uid, gid):
+        data_in = cherrypy.request.body.read()
+        data_json = json.loads(data_in)
+
+        if(gid not in self.game_db):
+            output = {'result':'Error', 'errors':["Game does not exist"]}
+            return json.dumps(output, encoding='latin-1')
         
         if(uid != self.game_db[gid]['guesser_uid']):
-            output = {'result':'Failure', 'errors':["Must be the guessing user to guess"]}
+            output = {'result':'Error', 'errors':["Must be the guessing user to guess"]}
             return json.dumps(output, encoding='latin-1')
 
         guess = data_json['guess']
@@ -43,6 +51,9 @@ class Game_Handler():
         return json.dumps(output,encoding='latin-1')
 
     def guess_phrase(self, gid, phrase):
+        if gid not in self.game_db:
+            return json.dumps({'result':'Error', 'errors':["Game does not exist"]})
+
         game_dict = self.game_db[gid]
         if phrase not in game_dict[guessed_phrases]:
             if phrase == game_dict['answer']:
@@ -69,7 +80,7 @@ class Game_Handler():
 
         # Logic Error: No active game with this gid
         else:
-            output = {'result': 'Error', 'message': 'This game was not requested by two players'}
+            output = {'result': 'Error', 'errors': ['This is not an active game id.']}
 
         return json.dumps(output, encoding='latin-1')
     
@@ -100,7 +111,7 @@ class Game_Handler():
             self.waiting_gids.append((new_gid, uid))
             waiting = True
 
-        output = {'gid': new_gid, 'waiting': waiting}
+        output = {'gid': new_gid, 'waiting': waiting, 'errors':[]}
         return json.dumps(output, encoding='latin-1')
 
     def post_game_prompt(self, gid):
@@ -116,7 +127,7 @@ class Game_Handler():
                 self.game_db[gid]['answer'] = answer
                 output = {'result': 'Success', 'message': 'Your game will begin shortly!'}
             else:
-                output = {'result': 'Failure', 'message': 'Your phrase must be between 3 and 30 alphabetical characters.'}
+                output = {'result': 'Error', 'message': 'Your phrase must be between 3 and 30 alphabetical characters.'}
         return json.dumps(output, encoding='latin-1')
 
     def assign_player_roles(self, uid1, uid2):
