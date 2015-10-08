8d056a066e584e733d41312e61141d19254a2af9
Norman S. Morales <norman.513@gmail.com>
2015-10-08 13:28:49 -0400

    Fixing merge conflict

--- page_handler.py
+++ page_handler.py
@@ -88,11 +88,11 @@ class Page_Handler():
                 word_progress.append(letter)
             else:
                 word_progress.append("_")
-		word_progress = "".join(word_progress)
+
         num_wrong = len(game_dict['incorrect_letters']) + len(game_dict['incorrect_words'])
         img_name = "../img/gallows"+str(num_wrong)+".png"
         
-        return env.get_template('Game.html').render(game_dict=game_dict, alphabet=alphabet, word_progress=word_progress, img_name=img_name)
+        return env.get_template('game.html').render(game_dict=game_dict, alphabet=alphabet, word_progress=word_progress, img_name=img_name)
     
     def get_wait_html(self, uid, gid):
         return env.get_template('Wait.html').render(uid=uid, gid=gid)
