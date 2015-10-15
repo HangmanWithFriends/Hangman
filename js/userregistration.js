c5c1f78e639211f8975b84d24a6a3d480f17f8b0
Natalie Sanders <nsander1@nd.edu>
2015-10-14 22:16:23 -0400

    userregistration: Added password confirmation & error messages. Debugged valid email/password checking

--- js/userregistration.js
+++ js/userregistration.js
@@ -1,7 +1,9 @@
 $(function() {
   function isValidEmail(email) {
     // var re = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-	var re = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z0-9_.+-]+$/;
+	  var re = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z0-9_.+-]+$/;
+    console.log("in valid email")
+    console.log(re.test(email))
     return re.test(email);
   }
 
@@ -9,54 +11,82 @@ $(function() {
     // at least one number, one letter
     // at least nine characters
     var re = /(?=.*\d)(?=.*[a-zA-z]).{7,}/;
+    console.log("in valid password")
+    console.log(re.test(pwd))
     return re.test(pwd);
   }
+
+  function isPasswordMatch(pwd, pwd_confirm) {
+    console.log("in password match")
+    console.log(pwd == pwd_confirm )
+    return pwd == pwd_confirm    
+  }
+
 
   $('#register-form').on('submit', function(event) { // form id
 	  
 	event.preventDefault();
 
 	var name = $('#user-name-input').val(); // input id
-    var email = $('#user-email-input').val(); // input id
-    var pwd = $('#user-password-input').val(); // input id
+  var email = $('#user-email-input').val(); // input id
+  var pwd = $('#user-password-input').val(); // input id
+  var pwd_confirm = $('#user-password-confirm-input').val();
+  var post = true;
+  
+  document.getElementById("errorbox").style.color = "Red";
 
-    if(isValidEmail(email)) {
-      $('#email-error').hide(); // div id
-    } else {
-      $('#email-error').text('Email must be in the correct format.').show();
-      event.preventDefault();
-    }
+  if(!isValidEmail(email)) {
+    $('#user-email-input').val('')
+    document.register.usermail.focus()
+    document.getElementById("errorbox").innerHTML = "Email must be in the correct format."
+    event.preventDefault();
+    post = false;
+  }
 
-    if(isValidPassword(pwd)) {
-      $('#password1-error').hide(); // div id
-    } else {
-      $('#password1-error').text('Password has to be 7 or more characters, and contain at least 1 number, and 1 letter.').show();
-      event.preventDefault();
-    }
+  if(!isValidPassword(pwd)) {
+    $('#user-password-input').val('')
+    $('#user-password-confirm-input').val('')
+    document.register.password.focus()
+    document.getElementById("errorbox").innerHTML = "Password has to be 7 or more characters, and contain at least 1 number, and 1 letter."
+    event.preventDefault();
+    post = false;
+  }
+
+  if(!isPasswordMatch(pwd, pwd_confirm)) {
+    $('#user-password-input').val('')
+    $('#user-password-confirm-input').val('')
+    document.register.password.focus()
+    document.getElementById("errorbox").innerHTML = "Passwords don't match"
+    event.preventDefault();
+    post = false;
+  }
+
+  if(post){
     $.ajax({
-            type : 'POST',
-            url: 'register',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                usermail: email,
-                password: pwd,
-                username: name
-            }),
-            dataType: 'json'
-        }).done(function(data) {
-			var d = data;
-			if(d.errors.length > 0){
-				showAlert('Username/password combination is wrong', 'danger');
-			}
-			else{
-				
-				window.location.href = "/lobby";
-			}
-			
-		}).fail(function() {
-			showAlert('Username/password combination is wrong.', 'danger');
-		});
+          type : 'POST',
+          url: 'register',
+          contentType: 'application/json',
+          data: JSON.stringify({
+              usermail: email,
+              password: pwd,
+              username: name
+          }),
+          dataType: 'json'
+      }).done(function(data) {
+          console.log('done posting');
+          var d = data;
+          if(d.errors.length > 0){
+            showAlert('Username/password combination is wrong', 'danger');
+          }
+          else{
+            
+            window.location.href = "/lobby/1";
+          } 
+      }).fail(function() {
+          showAlert('Username/password combination is wrong.', 'danger');
+      });
+  }
 
-	    });
-    });
+});
+});
 
