7dde1c94afa1d7616efb364ff491bfcfc40bf897
Natalie Sanders <nsander1@nd.edu>
2015-10-16 14:07:47 -0400

    fixed casting error (again)

--- page_handler.py
+++ page_handler.py
@@ -29,7 +29,7 @@ class Page_Handler():
         return env.get_template('Lobby.html').render(uid=uid, display_name=display_name, avatar=avatar)
 
     def get_request_phrase_html(self, uid, gid):
-        guesser_name = self.users[uid]['name']
+        guesser_name = self.users[uid]['username']
         return env.get_template('RequestPhrase.html').render(uid=uid, gid=gid, guesser_name=guesser_name)
 
     def get_game_html(self, uid, gid):
@@ -44,7 +44,7 @@ class Page_Handler():
     
     def get_guest_uid(self):
         userid = "g" + str(self.next_guest_user)
-        self.all_users[userid] = {"name" : "guest"}
+        self.all_users[userid] = {"username" : "guest"}
         self.next_guest_user += 1
         guest_info = {'uid' : userid}
         guest_info['errors'] = []
@@ -72,7 +72,8 @@ class Page_Handler():
         template page. 
         
         '''
-        
+        '''
+        gid = int(gid)
         game_state = requests.get('http://localhost:8080/game/'+str(gid))
  
         game_dict = json.loads(game_state.content)
@@ -80,11 +81,13 @@ class Page_Handler():
             return game_dict['errors']
         
         '''
+
+        gid = int(gid)
         if gid not in self.db['games']:
                 return ['This is not an active game id.']
 
         game_dict = self.db['games'][gid]
-        '''
+        
         alphabet = list(string.ascii_uppercase)
 
         list_word_progress = []
@@ -105,15 +108,15 @@ class Page_Handler():
         #These would be the users display names
         creator_uid = self.db['games'][gid]['creator_uid']
         guesser_uid = self.db['games'][gid]['guesser_uid']
-        creator_name = self.db['users'][creator_uid]['name']
-        guesser_name = self.db['users'][guesser_uid]['name']
+        creator_name = self.db['users'][creator_uid]['username']
+        guesser_name = self.db['users'][guesser_uid]['username']
         
 		
         if(str(uid) == str(game_dict['creator_uid'])):
-            return env.get_template('SpectatorGame.html').render(game_dict=game_dict, alphabet=alphabet, word_progress=word_progress, img_name=img_name, gid=gid, uid=uid, guesser_name = guesser_name, creator_name = creator_name)
+            return env.get_template('SpectatorGame.html').render(game_dict=game_dict, alphabet=alphabet, word_progress=word_progress, img_name=img_name, gid=str(gid), uid=uid, guesser_name = guesser_name, creator_name = creator_name)
 
  
-        return env.get_template('Game.html').render(game_dict=game_dict, alphabet=alphabet, word_progress=word_progress, img_name=img_name, gid=gid, uid=uid, guesser_name = guesser_name, creator_name = creator_name)
+        return env.get_template('Game.html').render(game_dict=game_dict, alphabet=alphabet, word_progress=word_progress, img_name=img_name, gid=str(gid), uid=uid, guesser_name = guesser_name, creator_name = creator_name)
     
     def get_wait_html(self, uid, gid):
         return env.get_template('Wait.html').render(uid=uid, gid=gid)
