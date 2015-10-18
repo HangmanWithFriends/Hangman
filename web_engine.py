919adb0fd2f278d91e76d4bd2635e7e5ad0d6d5a
user <rboccabe@nd.edu>
2015-10-18 09:51:51 -0400

    web_engine.py prefactoring: put dispatcher.connect()'s into functions per handler for future concept location's sake

--- web_engine.py
+++ web_engine.py
@@ -70,31 +70,37 @@ def start_service():
     account_handler = Account_Handler(db)
 
     thread.start_new_thread(auto_save, (db,))
+
+    connect_page_handler_dispatches(dispatcher, page_handler)
+    connect_game_handler_dispatches(dispatcher, game_handler)
+    connect_account_handler_dispatches(dispatcher, account_handler)
     
+    cherrypy.quickstart(app)
+    
+def connect_page_handler_dispatches(dispatcher, page_handler):
     dispatcher.connect('default_login','/',controller=page_handler,action='get_login_html',conditions=dict(method=['GET']))
     dispatcher.connect('get_login_page','/login',controller=page_handler, action='get_login_html',conditions=dict(method=['GET']))
     dispatcher.connect('get_lobby_page','/lobby/{uid}',controller=page_handler, action='get_lobby_html',conditions=dict(method=['GET']))
     dispatcher.connect('get_request_phrase_page','/phrase/{uid}/{gid}',controller=page_handler, action='get_request_phrase_html',conditions=dict(method=['GET']))
-    dispatcher.connect('handle_login','/login',controller=account_handler, action='handle_login_request',conditions=dict(method=['POST']))
-    #dispatcher.connect('get_game_page','/gameplay/{uid}/{gid}/other',controller=page_handler, action='get_game_html',conditions=dict(method=['GET']))
     dispatcher.connect('get_register_page', '/register', controller=page_handler, action='get_register_html',conditions=dict(method=['GET']))
-    dispatcher.connect('handle_register','/register',controller=account_handler, action='handle_register_request',conditions=dict(method=['POST']))
     dispatcher.connect('get_guest_lobby_page','/guestlobby/{uid}',controller=page_handler, action='get_guest_lobby_html',conditions=dict(method=['GET']))
     dispatcher.connect('get_guest_request_phrase_page','/guestphrase/{uid}/{gid}',controller=page_handler,action='get_guest_request_phrase_html',conditions=dict(method=['GET']))
     dispatcher.connect('get_guest_game_page','/guestgame/{uid}/{gid}',controller=page_handler,action='get_guest_game_html',conditions=dict(method=['GET']))
-    dispatcher.connect('get_dummy_game_JSON', '/dummygame/{gid}',controller=game_handler,action='get_dummy_game',conditions=dict(method=['GET']))
     dispatcher.connect('get_gameplay_page', '/gameplay/{uid}/{gid}',controller=page_handler,action='get_gameplay_html',conditions=dict(method=['GET']))
-    dispatcher.connect('get_game_JSON', '/game/{gid}',controller=game_handler,action='get_game',conditions=dict(method=['GET']))
+    dispatcher.connect('get_wait_page', '/gameplay/{uid}/wait/{gid}', controller=page_handler, action='get_wait_html', conditions=dict(method=['GET']))
 
-    dispatcher.connect('get_guest_uid','/get-guest-uid',controller=account_handler,action='get_guest_uid',conditions=dict(method=['GET']))
-    
+def connect_game_handler_dispatches(dispatcher, game_handler):
+    dispatcher.connect('get_dummy_game_JSON', '/dummygame/{gid}',controller=game_handler,action='get_dummy_game',conditions=dict(method=['GET']))
     dispatcher.connect('post_game_prompt', '/game/{uid}/prompt/{gid}',controller=game_handler,action='post_game_prompt',conditions=dict(method=['POST']))
     dispatcher.connect('post_game_request', '/game/{uid}/request',controller=game_handler,action='post_game_request',conditions=dict(method=['POST']))
     dispatcher.connect('get_game_request','/game/{uid}/request',controller=game_handler, action='get_game_request',conditions=dict(method=['GET']))
     dispatcher.connect('post_guess_JSON', '/game/{uid}/{gid}', controller=game_handler,action='post_guess',conditions=dict(method=['POST']))
-    dispatcher.connect('get_wait_page', '/gameplay/{uid}/wait/{gid}', controller=page_handler, action='get_wait_html', conditions=dict(method=['GET']))
-    
-    cherrypy.quickstart(app)
-        
+    dispatcher.connect('get_game_JSON', '/game/{gid}',controller=game_handler,action='get_game',conditions=dict(method=['GET']))
+
+def connect_account_handler_dispatches(dispatcher, account_handler):
+    dispatcher.connect('handle_login','/login',controller=account_handler, action='handle_login_request',conditions=dict(method=['POST']))
+    dispatcher.connect('handle_register','/register',controller=account_handler, action='handle_register_request',conditions=dict(method=['POST']))
+    dispatcher.connect('get_guest_uid','/get-guest-uid',controller=account_handler,action='get_guest_uid',conditions=dict(method=['GET']))
+
 if __name__ == '__main__':
     start_service()
