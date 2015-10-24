2bc356a2a6b2ed9715f942cccc598c89b96e4ba6
user <rboccabe@nd.edu>
2015-10-23 20:06:18 -0400

    Add helper for getting commonly rendered variables, begin adding friend search functionality

--- page_handler.py
+++ page_handler.py
@@ -26,11 +26,10 @@ class Page_Handler():
         return env.get_template('Home.html').render()
     
     def get_lobby_html(self, uid):
-        display_name = self.users[uid]["username"]
-        avatar = self.images_path + self.get_image_name_from_uid(uid)
-        friends = self.get_friend_info_tuples_from_uid(uid)
+        uid_info = self.get_info_dict_from_uid(uid)
+        friends = self.get_friend_info_dicts_from_uid(uid)
         num_requests = len(self.users[uid]['incoming_friend_requests'])
-        return env.get_template('Lobby.html').render(uid=uid, display_name=display_name, avatar=avatar, friends=friends, num_requests=num_requests)
+        return env.get_template('Lobby.html').render(uid=uid_info['uid'], display_name=uid_info['username'], avatar=uid_info['profile_image'], friends=friends, num_requests=num_requests)
 
     def get_request_phrase_html(self, uid, gid):
         guesser_uid = self.db['games'][gid]['guesser_uid']
@@ -51,11 +50,10 @@ class Page_Handler():
         return env.get_template('GuestRequestPhrase.html').render()
 
     def get_guest_lobby_html(self, uid):
-        display_name = self.users[uid]["username"]
-        avatar = self.images_path + self.default_image_name 
+        uid_info = self.get_info_dict_from_uid(uid) 
         if uid in self.users.keys() and 'g' not in uid:                    
-            return env.get_template('Lobby.html').render(uid=uid, display_name=display_name, avatar=avatar)   
-        return env.get_template('GuestLobby.html').render(uid=uid,display_name=display_name)
+            return env.get_template('Lobby.html').render(uid=uid, display_name=uid_info['username'], avatar=uid_info['profile_image'])   
+        return env.get_template('GuestLobby.html').render(uid=uid,display_name=uid_info['username'])
     
     
     def get_guest_game_html(self, uid, gid):
@@ -109,23 +107,53 @@ class Page_Handler():
         return env.get_template('Wait.html').render(uid=uid, gid=gid)
     
     def get_settings_html(self, uid):
-        display_name = self.users[uid]["username"]
-        avatar = self.images_path + self.get_image_name_from_uid(uid)
-        email = self.users[uid]["usermail"]
+        uid_info = self.get_info_dict_from_uid(uid)
+        return env.get_template('Settings.html').render(uid=uid, display_name=uid_info['username'], avatar=uid_info['profile_image'], email=uid_info['usermail'])
+
+    def handle_friends_management_search_html(self, uid):
+        cl = cherrypy.request.headers['Content-Length']
+        data_json = cherrypy.request.body.read(int(cl))
+        incoming_data = json.loads(data_json)
+
+        search_string = None
+        if 'search_string' in incoming_data:
+            search_string = incoming_data['search_string']
+
+        return self.get_friends_management_html(uid, search_string)
+
+    def get_friends_management_html(self, uid, search_string=None):
+        search_results_list = []
+        pending_requests_list = []
+
+        search_results = self.search_for_friends(search_string) 
+        pending_request_uids = self.users[uid]['incoming_friend_requests']
+
+        for pending_request_uid in pending_requests_uids:
+            pending_requests_info_dicts_list.append(self.get_info_dict_from_uid(pending_request_uid))
         
-        return env.get_template('Settings.html').render(uid=uid, display_name=display_name, avatar=avatar, email=email)
-   
-    def get_friend_info_tuples_from_uid(self, uid):
+        for search_result_uid in search_result_uids:
+            search_result_info_dicts_list.append(self.get_info_dict_from_uid(search_result_uid))
+
+        uid_info = self.get_info_dict_from_uid(uid)
+        return env.get_template('ManageFriends.html').render(uid=uid, display_name=uid_info['username'], avatar=uid_info['profile_image'], pending_requests=pending_requests_info_dicts, search_results=search_result_info_dicts, search_string=search_string)
+
+    def get_friend_info_dicts_from_uid(self, uid):
         if uid not in self.db['users']:
             return []
 
         friends_uids = self.users[uid]['friends']
-        friends_tuples_list = []
+        friends_dicts_list = []
         for f_uid in friends_uids:
-            friends_tuples_list.append(f_uid, self.users[f_uid]['username'], self.images_path+self.get_image_name_from_uid(f_uid))
+            friends_dicts_list.append(self.get_info_dict_from_uid(f_uid))
 
-        return friends_tuples_list
+        return friends_dicts_list
     
+    def get_info_dict_from_uid(self, uid):
+        to_return = {'uid':uid, 'username':self.users[uid]['username'], 'profile_image':self.images_path+self.get_image_name_from_uid(uid)}
+        if uid[0] != 'g':
+            to_return['usermail'] = self.users[uid]['usermail']
+        return to_return
+
     def get_image_name_from_uid(self, uid):
         image_name = self.default_image_name
         if uid not in self.db['users']:
@@ -137,3 +165,36 @@ class Page_Handler():
                 image_name = self.users[uid]['profile_image']
 
         return image_name
+
+    
+    def search_for_friends(self, uid, search_string):
+        return_uids = set()
+       
+        words = search_string.split("\s")
+
+        for word in words:
+            if word in self.db['username_words_to_uids']:
+                #multiple usernames could include the same word
+                uids = self.db['username_words_to_uids'][word]
+                for found_uid in uids:
+                    return_uids.add(found_uid)
+
+        #if it's an email, search by email, it's unlikely but possible that the above
+        #adds any uids, as an email would need to be part of someones username
+        if regex.match(".*@*.*", search_string):
+            if search_string in self.emails_to_uids:
+                return_uids.add(self.emails_to_uids[search_string])
+    
+        #search by first three letters of each word 
+        else:
+            for word in words:
+                first_three = word[0:3]
+                if first_three in self.db['username_word_starts_to_uids']:
+                    uids = self.db['user_name_word_starts_to_uids']
+                    for found_uid in uids:
+                        return_uids.add(found_uid)         
+
+        # don't include users that they are already friends with, or the user themself
+        return_uids = return_uids - set(self.users[uid]['friends'] + [uid])
+
+        return list(return_uids)
